198f4408fe2468fbfd2181b31322a1610c05db10
nesquik <rrtmshishe@gmail.com>
2026-06-07 23:31:55 +0300

    feat: add Task model and database setup

---
+++ models.py
@@ -0,0 +1,16 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, Boolean, String
+from datetime import datetime, timezone
+from database import Base
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
+    description: Mapped[str | None]
+    completed: Mapped[bool] = mapped_column(Boolean, default=False)
+    created_at: Mapped[datetime] = mapped_column(
+                DateTime, 
+                default=lambda: datetime.now(timezone.utc))
+    
